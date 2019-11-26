c930ea65d53a1dba8770a62881edb1cfccd022b6
boxlab <boxlab@foxmail.com>
2019-11-26 16:35:22 +0800

    [新增]简单首页完成，包含服务器IP和版本

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-
+    <div class="title">
+      <h1>End of The Century</h1>
+      <p>“末世纪” 纯净综合服务器</p>
+      <div class="mdui-container etcs-container container" style="background: rgba(255,255,255,.5);">
+        <div class="mdui-table-fluid mdui-typo">
+          <table class="mdui-table mdui-table-hoverable">
+            <thead>
+            <tr>
+              <th>IP</th>
+              <th>版本</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <td>etcs.redneno.me</td>
+              <td>1.14.4</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +32,44 @@
 </script>
 
 <style scoped>
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 80%;
+    height: calc(100vh - 50px);
+    margin: 0 auto;
+  }
+
+  .title h1 {
+    color: #FFFFFF;
+    margin: 0 auto;
+    font-size: 2.2rem;
+    font-weight: 100;
+  }
+
+  .title p {
+    color: #FFFFFF;
+  }
+
+  .container {
+    width: unset;
+    padding: 1rem;
+    margin-top: 2rem;
+  }
+
+
+  .mdui-table-fluid {
+    border: none;
+    box-shadow: none;
+  }
+
+  .mdui-table {
+    background: rgba(0, 0, 0, 0);
+  }
+
+  .mdui-table td, .mdui-table th {
+    border-bottom: 1px solid rgba(0, 0, 0, .05);
+  }
 </style>
